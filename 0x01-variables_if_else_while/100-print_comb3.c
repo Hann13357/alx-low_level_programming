ca74f5cb14a08afc36357d7c82535a7561773d6b
Hann13357 <122867177+Hann13357@users.noreply.github.com>
2023-02-17 22:11:52 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,16 +12,16 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '9'; ones++)/*print ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/* print ones digit*/
 		{
-			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+			if (!((ones == tens) || (tens > ones)))/* eliminates repitition*/
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == '9' && tens == '8'))/*addes comma and space*/
+				if (!(ones == '9' && tens == '8'))/* addes comma and space*/
 				{
 					putchar(',');
-					putchar(' '),
+					putchar(' ');
 				}
 			}
 		}
