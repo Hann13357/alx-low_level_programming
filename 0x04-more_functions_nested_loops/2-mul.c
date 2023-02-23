1bb691fb137ae5749ad8f8061bb1f838838bfe36
Hanna <marsilas784@gmail.com>
2023-02-23 01:16:29 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - check the code
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
